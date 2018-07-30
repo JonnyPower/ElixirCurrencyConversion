238671a8520a2541c9d46982f3098e1e11aac7aa
jonnypower <me@jonnypower.com>
2018-07-29 18:31:05 -0700

    feat: Initial

---
+++ lib/currency_conversion.ex
@@ -0,0 +1,18 @@
+defmodule CurrencyConversion do
+  @moduledoc """
+  Documentation for CurrencyConversion.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CurrencyConversion.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
